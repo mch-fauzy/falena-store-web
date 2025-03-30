e91903f8b877d62f65af1b7c9513e46959f5b301
mch-fauzy <mucha.fauzy@gmail.com>
2025-03-31 00:41:28 +0700

    docs: add comment

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,4 +1,4 @@
-/* Using route group to opting specific segments into a layout and organize route without affecting URL path, in this case is for path / and /product */
+/* Using route group to opting specific segments into a layout and organize route without affecting URL path, in this case is for path / (because have page.tsx inside home route group) and /product */
 
 import {Footer} from '@/components/shared/footer/footer';
 import {Header} from '@/components/shared/header/header';
